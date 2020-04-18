a8e462424746343397b424b397e10e7a70dfec83
Clayton Fellipe <c.fellipe18@gmail.com>
2020-04-18 00:36:19 -0300

    adding swagger in project

--- src/main/kotlin/com/photoapi/photoapi/controller/UserController.kt
+++ src/main/kotlin/com/photoapi/photoapi/controller/UserController.kt
@@ -2,6 +2,7 @@ package com.photoapi.photoapi.controller
 
 import com.photoapi.photoapi.entity.dto.UserDTO
 import com.photoapi.photoapi.service.UserService
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/user")
 data class UserController(val userService: UserService) {
 
+    @ApiOperation(value = "Create new user")
     @PostMapping()
     fun createUser(@RequestBody user: UserDTO): ResponseEntity<UserDTO> {
         return ResponseEntity.ok().body(userService.saveUser(user))
